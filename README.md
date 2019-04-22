8476d70fb89dabec3a719d4b0912712c499003c9
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2019-04-21 20:28:46 -0400

    Actualizacion de generados

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Computación Gráfica
-## Gilberto Espinoza
-### Universidad de Santiago de Chile
 
 En este repositorio manejare lo que es el curso electivo de Computación Gráfica impartido en USACH en la ciudad de Santiago Chile.
 
-## Secciones
-1. Primitivas Graficas
+
+## Tabla de Contenidos
+
+1. Primitivas Graficas.
+    1.1. Lineas
+    1.2. Circulos y Elipses
+    1.3. Parabolas
+    1.4. Poligonos
+    1.5. Relleno de poligonos
 
 ## Instalacion.
 Este proyecto utiliza la libreria `graphics.h` que no se encuentra en el estándar de C++. Asi que la agregaremos a nuestro compilador y haremos un link dentro de Code::Blocks para que nuestro proyecto pueda ser compilado.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <graphics.h>
 
+//#include "Punto.h"
+#include "Linea.h"
 int main(){
     std::cout << "Computacion Grafica." << std::endl;
 
+    /// DRAWING
+    // gm is el modo Grafico del display de la computadora
+    // mode genera imagenes usando pixels
+    // DETECT es un macro definido en "graphics.h" header
+    int gd = DETECT, gm;
+
+    // initgraph inicializa el grafico del sistema
+    // cargando los graficos del disco
+    initgraph(&gd, &gm, "");
+
+    //Punto::test(); Dibuja 3 puntos
+    Linea::test();
+    // End DRAWING
+    getch();
+    // Cierra el grafico generado y limpia la memoria.
+    closegraph();
     return 0;
 }
